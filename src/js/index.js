82e557ada8493e61795faafa8ebd1d81bbb2d8ed
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-08-07 22:39:53 +0100

    added contact form, but having bugs in description textarea

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
 import '../scss/main.scss';
+const flipper = document.getElementById('about__flipper');
+document.getElementById('about__contact-btn').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (!flipper.checked) {
+    flipper.checked = true;
+  }
+});
+
+document.getElementById('about__form-return').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (flipper.checked) {
+    flipper.checked = false;
+  }
+});
