b40e47eeabc615aa8674e691472353fd6ba5eaaa
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-08-09 13:51:26 +0100

    completed review slider functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
 import '../scss/main.scss';
 const flipper = document.getElementById('about__flipper');
+const leftReviewBtn = document.getElementById('reviews__navigation-listitem-firstrow');
+const rightReviewBtn = document.getElementById('reviews__navigation-listitem-secondrow');
+const reviewFlipper = document.getElementById('reviews__flipper');
+
 document.getElementById('about__contact-btn').addEventListener('click', function (e) {
   e.preventDefault();
   if (!flipper.checked) {
@@ -13,3 +17,12 @@ document.getElementById('about__form-return').addEventListener('click', function
     flipper.checked = false;
   }
 });
+
+[leftReviewBtn, rightReviewBtn].forEach(btn => {
+  btn.addEventListener('click', function () {
+    if (btn.id === 'reviews__navigation-listitem-secondrow') {
+      return reviewFlipper.checked = true;
+    }
+    reviewFlipper.checked = false;
+  })
+})
