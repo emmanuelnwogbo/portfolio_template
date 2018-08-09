8b1580bd1a644d8af56b0ca5b49dadfccf5a04e0
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-08-09 16:27:54 +0100

    refactored if statements to ternery statements in index.js, for review slider functionality and contact form flip feature

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,21 @@
 import '../scss/main.scss';
-const flipper = document.getElementById('about__flipper');
-const leftReviewBtn = document.getElementById('reviews__navigation-listitem-firstrow');
-const rightReviewBtn = document.getElementById('reviews__navigation-listitem-secondrow');
-const reviewFlipper = document.getElementById('reviews__flipper');
+const aboutflipper = document.getElementById('about__flipper'),
+  aboutContactBtn = document.getElementById('about__contact-btn'),
+  aboutFormReturnBtn = document.getElementById('about__form-return'),
+  leftReviewBtn = document.getElementById('reviews__navigation-listitem-firstrow'),
+  rightReviewBtn = document.getElementById('reviews__navigation-listitem-secondrow'),
+  reviewFlipper = document.getElementById('reviews__flipper');
 
-document.getElementById('about__contact-btn').addEventListener('click', function (e) {
-  e.preventDefault();
-  if (!flipper.checked) {
-    flipper.checked = true;
-  }
-});
-
-document.getElementById('about__form-return').addEventListener('click', function (e) {
-  e.preventDefault();
-  if (flipper.checked) {
-    flipper.checked = false;
-  }
+[aboutContactBtn, aboutFormReturnBtn].forEach(btn => {
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    this.id !== 'about__form-return' ? aboutflipper.checked = true : aboutflipper.checked = false;
+  });
 });
 
 [leftReviewBtn, rightReviewBtn].forEach(btn => {
-  btn.addEventListener('click', function () {
-    if (btn.id === 'reviews__navigation-listitem-secondrow') {
-      return reviewFlipper.checked = true;
-    }
-    reviewFlipper.checked = false;
-  })
-})
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    this.id === 'reviews__navigation-listitem-secondrow' ? reviewFlipper.checked = true : reviewFlipper.checked = false;
+  });
+});
